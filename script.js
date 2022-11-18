45d9cc3a12d08cda648d0e498f16549df26155af
Leissaa <118523067+Leissaa@users.noreply.github.com>
2022-11-18 18:25:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer", "Financial Literate", "Android Developer"],
+    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer""],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer", "Financial Literate", "Android Developer"],
+    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
